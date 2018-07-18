e364afdfe8e4166c51664bb156e84551e035945b
marla kicklighter <mrskicklighter@gmail.com>
2018-07-18 19:52:37 -0400

    overlay menu and back to top button

--- jss/script.js
+++ jss/script.js
@@ -1,3 +1,14 @@
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+
 /* Main Nav Dropdown menu--------------------------------------------
 When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
